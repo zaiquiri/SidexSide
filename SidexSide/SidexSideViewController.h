342ecaa4ac53d59a8a9e3aa008ec9963b388bee0
zaiquiri <zachary.lindberg@gmail.com>
2014-08-31 08:55:08 -0400

    initial commit

---
+++ SidexSide/SidexSideViewController.h
@@ -0,0 +1,13 @@
+//
+//  SidexSideViewController.h
+//  SidexSide
+//
+//  Created by zach on 8/31/14.
+//  Copyright (c) 2014 zappery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SidexSideViewController : UIViewController
+
+@end
---
+++ SidexSide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SidexSide
+//
+//  Created by zach on 8/31/14.
+//  Copyright (c) 2014 zappery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SidexSideAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SidexSideAppDelegate class]));
+    }
+}
