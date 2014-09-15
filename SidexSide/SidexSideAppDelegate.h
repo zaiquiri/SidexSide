a91e697223f3bbed9b74474a57fe7130cecba186
zaiquiri <zachary.lindberg@gmail.com>
2014-09-15 10:09:53 -0400

    deleted auto-gen comments

--- SidexSide/SidexSideAppDelegate.h
+++ SidexSide/SidexSideAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  SidexSideAppDelegate.h
-//  SidexSide
-//
-//  Created by zach on 8/31/14.
-//  Copyright (c) 2014 zappery. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SidexSideAppDelegate : UIResponder <UIApplicationDelegate>
--- SidexSide/main.m
+++ SidexSide/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  SidexSide
-//
-//  Created by zach on 8/31/14.
-//  Copyright (c) 2014 zappery. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "SidexSideAppDelegate.h"
