e7d8feaf61bd650491b3c06a43a48b8521382875
zaiquiri <zachary.lindberg@gmail.com>
2014-10-01 11:46:23 -0400

    set up basic layout for ScenePartnerFoundViewController

--- SidexSide/ScenePartnerFoundViewController.m
+++ SidexSide/ScenePartnerFoundViewController.m
@@ -1,5 +1,14 @@
 #import "ScenePartnerFoundViewController.h"
 
+@interface ScenePartnerFoundViewController()
+
+@property (strong, nonatomic) IBOutlet UIView *flipContainer;
+@property (strong, nonatomic) IBOutlet UIImageView *headshotView;
+@property (strong, nonatomic) IBOutlet UIScrollView *additionalInformationView;
+
+
+@end
+
 @implementation ScenePartnerFoundViewController
 
 @end
