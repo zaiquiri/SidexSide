ece4d57176edbbc11c261012ee7a21aabd634930
zaiquiri <zachary.lindberg@gmail.com>
2014-10-03 13:30:19 -0400

    Started to put together waiting for confirm view

---
+++ SidexSide/WaitingForConfirmationViewController.m
@@ -0,0 +1,12 @@
+#import "WaitingForConfirmationViewController.h"
+
+@interface WaitingForConfirmationViewController()
+
+@property (strong, nonatomic) IBOutlet UIView *flipContainer;
+@property (strong, nonatomic) IBOutlet UIView *infoView;
+
+@end
+
+@implementation WaitingForConfirmationViewController
+
+@end
